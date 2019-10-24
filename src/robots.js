ba706bc0f262e4395e62907e18d187ccbe0db93a
ashmeet <ashmeetgoyal1999@gmail.com>
2019-10-24 21:12:19 +0530

    robofriends webpage..

---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots =[
+{
+	id:1,
+	name:'Ashmeet Goyal',
+	username:'ashmeet',
+	email:'ashmeet@gmail.ty',
+},
+{
+	id:2,
+	name:'Naman Goyal',
+	username:'naman',
+	email:'nono@yohoo.cm',
+},
+{
+	id:3,
+	name:'Aryan Taneja',
+	username:'tanyAr',
+	email:'taneja@hipe.co',
+},
+{
+	id:4,
+	name:'Tarun Singal',
+	username:'tarun',
+	email:'truntrun@yup.hp',
+},
+{
+	id:5,
+	name:'Himanshu Aggarwal',
+	username:'hamu',
+	email:'giyaan@mail.com',
+},
+{
+	id:6,
+	name:'Ujjwal Singh',
+	username:'ujjwalrock',
+	email:'rockstar@gmco.nm',
+},
+{
+	id:7,
+	name:'Umesh',
+	username:'garg', 
+	email:'umgarg@stack.com',
+},
+]
